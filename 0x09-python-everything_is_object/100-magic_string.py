9844a06dc347a38add5427a92b3e70feeeb6c8ec
Jola <nitariod@gmail.com>
2023-01-03 18:21:11 +0100

    0x09 100 attempt 2

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -2,5 +2,3 @@
 def magic_string():
     magic_string.count = getattr(magic_string, 'count', 0) + 1
     return ("BestSchool, " * (magic_string.count - 1) + "BestSchool")
-for i in range(10):
-    print(magic_string())
