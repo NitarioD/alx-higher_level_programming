768c17989318aa8fc02b4996260f8d6799303f57
Jola <nitariod@gmail.com>
2023-03-22 13:41:50 +0100

    0x0F 1 checker fix 2

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
-
+"""
+lists all states with starting name with N
+from the database hbtn_0e_0_usa
+"""
 
 import MySQLdb
 from sys import argv
 
-'''
-lists all states with starting name with N
-from the database hbtn_0e_0_usa
-'''
 if __name__ == "__main__":
     con = MySQLdb.connect(
         host="localhost", port=3306, user=argv[1],
         passwd=argv[2], db=argv[3])
     cursor = con.cursor()
     cursor.execute(
-            "SELECT * FROM states WHERE left(name,1)='N' ORDER BY id ASC")
+            "SELECT * FROM states WHERE left(name,1)='N'")
     db = cursor.fetchall()
     for i in db:
         print(i)
-    cursor.close()
-    con.close()
