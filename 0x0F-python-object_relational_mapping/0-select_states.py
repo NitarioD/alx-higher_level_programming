096f7ab27ccf35438a724b82caf24178f8dfc89b
Jola <nitariod@gmail.com>
2023-03-22 13:33:00 +0100

    0x0F 0 checker fix

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 
+"""
+a script that lists all the states from the database
+Usage: ./0-select_states.py <mysql username> <mysql password> <database>
+"""
 import MySQLdb
 from sys import argv
 
-"""
-a script that lists all the states from the database
-"""
 if __name__ == "__main__":
     con = MySQLdb.connect(
             host="localhost", port=3306, user=argv[1],
-            passwd=argv[2], database=argv[3])
+            passwd=argv[2], db=argv[3])
     cursor = con.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states")
     db = cursor.fetchall()
     for i in db:
         print(i)
